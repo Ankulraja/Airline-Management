f247d4dd907a401b9c1047c379097c5e847bd3a2
Ankulraja <154729128+Ankulraja@users.noreply.github.com>
2024-02-22 12:58:19 +0530

    2nd After Clone

---
+++ FrontEnd/src/Pages/AdminDashboad.js
@@ -0,0 +1,8 @@
+const AdminDashboad = () => {
+  return (
+    <div className='flex justify-center items-center text-black text-3xl h-[100vh] font-serif'>
+    Welcome to Airline Admin Dashboard.
+    </div>
+  )
+}
+export default AdminDashboad
